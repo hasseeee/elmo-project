66e332ccda9bceff0851ddf657342974f0842a3b
hasseeee <hasegawa.takuya1204@gmail.com>
2025-08-23 02:36:25 +0900

    hotfix: パッケージ

---
+++ internal/models/start-room.go
@@ -0,0 +1,15 @@
+package models
+
+// StartRoomResponse represents the response for starting a room
+type StartRoomResponse struct {
+	InitialQuestion string        `json:"initial_question"`
+	RoomInfo        RoomInfo      `json:"room_info"`
+	Participants    []Participant `json:"participants"`
+}
+
+// RoomInfo represents information about a room
+type RoomInfo struct {
+	RoomID string `json:"room_id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
